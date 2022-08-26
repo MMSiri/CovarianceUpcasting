79cf25273eed8884c6cbb54f6cf621464ca8c6a2
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-08-26 18:52:43 +0100

    Add project files.

---
+++ CovarianceUpcasting/KindOfDuck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovarianceUpcasting
+{
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Loon,
+    }
+}
